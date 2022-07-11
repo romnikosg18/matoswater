c30a4ba26d14a84307568ff8cbec908ecedc3c5e
Allen Joshua <49166420+TheDude19@users.noreply.github.com>
2022-07-12 01:11:01 +0800

    working login

--- index.php
+++ index.php
@@ -1,7 +1,74 @@
 <?php
 include "includes/db.php";
 include "includes/functions.php";
+session_start();
 
+if(isset($_POST['login'])) {
+	$username = $_POST['user'];
+	$password = $_POST['password'];
+
+	$username = mysqli_real_escape_string($connection, $username);
+	$password = mysqli_real_escape_string($connection, $password);
+
+	$query = "SELECT * FROM accounts WHERE username = '{$username}' ";
+	$select_user_query = mysqli_query($connection, $query);
+
+	if(!$select_user_query) {
+		die("Query in loginphp failed" . mysqli_error($connection) );
+	}
+
+	while($row = mysqli_fetch_array($select_user_query)){
+		$infoid = $row['id'];
+		$infoemail = $row['email'];
+		$infousername = $row['username'];
+		$infopassword = $row['password'];
+		$infotime = $row['timecreated'];
+	
+
+
+		$entereduser = $_POST['user'];
+		$enteredpass = $_POST['password'];
+
+		echo "Account information:" . "<br>";
+		echo "Username is: [" . $infousername . "]" . "<br>" ;
+		echo "Password is: [" . $infopassword . "]" . "<br>" . "<br>" ;
+		echo "Your entered Password is: [" . $enteredpass . "]" . "<br>" . "<br>";
+
+		if($entereduser == $infousername && $enteredpass == $infopassword){
+			$_SESSION['id'] = $infoid;
+			$_SESSION['email'] = $infoemail;
+			$_SESSION['username'] = $infousername;
+			$_SESSION['password'] = $infopassword;
+			$_SESSION['fname'] = $infofname;
+			$_SESSION['timecreated'] = $infotime;
+
+
+
+
+			//redirects you to homepage
+			header("Location: home.html");
+
+		} else if ($entereduser !== $infousername && $enteredpass !== $infopassword) {
+			echo "Invalid login details";
+
+		}
+//            else {
+//                echo "wrong login information" . "<br>" . "<br>";
+//            }
+
+//            if($username !== $infopassword && $password !==$infopassword){
+//                header("Location: login.php");
+//            }
+	}
+
+
+}
+
+else {
+	echo "Input user details to login" . "<br>" . "<br>";
+
+
+}
 ?>
 
 
